b3ecf7b5504008949bd518abe7e6fb2217261ff3
defnone <141522332+defnone@users.noreply.github.com>
2024-06-14 15:04:01 +0700

    feat: Add clean_after_days in Settings model

--- app/models.py
+++ app/models.py
@@ -105,7 +105,7 @@ db.Index("index_feed_item_read", FeedItem.read)
 class Settings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     update_interval = db.Column(db.Integer, nullable=False, default=60)
-    clean_interval = db.Column(db.Integer, nullable=False, default=60)
+    clean_after_days = db.Column(db.Integer, nullable=False, default=60)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     timezone = db.Column(db.String(50), nullable=False, default="UTC")
     language = db.Column(db.String(50), nullable=False, default="English")
