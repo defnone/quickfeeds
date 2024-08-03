40f9eb5be9208ae802e128b732172b78b47b5ca8
defnone <141522332+defnone@users.noreply.github.com>
2024-08-03 12:08:41 +0700

    add daily settings endpoint

--- app/api/v1/settings.py
+++ app/api/v1/settings.py
@@ -1,8 +1,11 @@
+from datetime import datetime, timedelta, UTC
+import logging
+import pytz
 from flask import Blueprint, jsonify, request
 from flask_login import current_user
 from werkzeug.security import generate_password_hash
 from app import db
-from app.models import Settings
+from app.models import Settings, User, Feed
 
 api_settings_blueprint = Blueprint("api_settings_blueprint", __name__)
 
@@ -36,6 +39,15 @@ def user_settings():
             )
         else:
             current_user_settings.groq_api_key = None
+
+        # If the user has an OpenAI API key, replace it with asterisks for
+        # security
+        if current_user_settings.openai_api_key:
+            current_user_settings.openai_api_key = (
+                "******************************"
+            )
+        else:
+            current_user_settings.openai_api_key = None
 
         # Return the user's settings in a JSON format
         return (
@@ -71,10 +83,7 @@ def user_settings():
         # Validate integer fields and set default value for clean_after_days
         if "clean_after_days" in update_data:
             clean_after_days = int(update_data["clean_after_days"])
-            if (
-                not isinstance(clean_after_days, int)
-                or clean_after_days < 1
-            ):
+            if not isinstance(clean_after_days, int) or clean_after_days < 1:
                 return (
                     jsonify({"error": "Not a valid value.'"}),
                     400,
@@ -86,13 +95,15 @@ def user_settings():
                 if hasattr(current_user_settings, key):
                     setattr(current_user_settings, key, value)
         else:
-            # If the user doesn't have settings, create them with default values
+            # If the user doesn't have settings,
+            # create them with default values
             default_values = {
                 "update_interval": 60,
                 "timezone": "UTC",
                 "language": "English",
                 "unread": True,
                 "groq_api_key": None,
+                "openai_api_key": None,
                 "translate": False,
                 "clean_after_days": current_user_settings.clean_after_days,
             }
@@ -118,6 +129,160 @@ def user_settings():
             ),
             200,
         )
+
+
+@api_settings_blueprint.route("/daily", methods=["GET", "POST"])
+def daily_settings():
+    # Get the current user from the session
+    if not current_user.is_authenticated:
+        return jsonify({"message": "User not authenticated."}), 401
+
+    # Query the database for the settings of the current user
+    current_user_settings = Settings.query.filter_by(
+        user_id=current_user.id
+    ).first()
+    user = User.query.filter_by(id=current_user.id).first()
+
+    if user.daily_sync_at is None:
+        now = datetime.now(UTC)
+        user.daily_sync_at = now.replace(
+            hour=7, minute=0, second=0, microsecond=0
+        )
+        if now.hour >= 7:
+            user.daily_sync_at += timedelta(days=1)
+
+    if user.daily_sync_at.tzinfo is None:
+        user.daily_sync_at = pytz.utc.localize(user.daily_sync_at)
+
+    user_timezone = pytz.timezone(current_user_settings.timezone)
+    daily_sync_at_user_tz = user.daily_sync_at.astimezone(user_timezone)
+    daily_sync_at = daily_sync_at_user_tz.strftime("%H:%M:%S")
+
+    if request.method == "GET":
+        feeds = Feed.query.filter_by(user_id=current_user.id).all()
+        feed_output = [feed.to_dict() for feed in feeds]
+
+        # Return the user's settings in a JSON format
+        return (
+            jsonify(
+                active=current_user_settings.daily_active,
+                hours_summary=current_user_settings.daily_hours_summary,
+                translate=current_user_settings.daily_translate,
+                last_sync_time_duration=(
+                    current_user_settings.daily_last_sync_time_duration
+                ),
+                process_read=current_user_settings.daily_process_read,
+                title_provider=current_user_settings.daily_title_provider,
+                compare_titles=current_user_settings.daily_compare_titles,
+                daily_sync_at=daily_sync_at,
+                feeds=feed_output,
+            ),
+            200,
+        )
+
+    elif request.method == "POST":
+        # Update the settings of the current user based on the
+        # data received from the POST request
+        active = request.json.get("active")
+        hours_summary = request.json.get("hours_summary")
+        translate = request.json.get("translate")
+        process_read = request.json.get("process_read")
+        compare_titles = request.json.get("compare_titles")
+        title_provider = request.json.get("title_provider")
+
+        sync_at = request.json.get("sync_at")
+        sync_time = datetime.strptime(sync_at, "%H:%M:%S").time()
+        current_date = datetime.now(user_timezone).date()
+        sync_datetime_user_tz = datetime.combine(current_date, sync_time)
+        sync_datetime_user_tz = user_timezone.localize(sync_datetime_user_tz)
+        sync_datetime_utc = sync_datetime_user_tz.astimezone(pytz.utc)
+
+        current_user_settings.daily_active = (
+            active
+            if active is not None
+            else current_user_settings.daily_active
+        )
+        current_user_settings.daily_hours_summary = (
+            hours_summary
+            if hours_summary is not None
+            else current_user_settings.daily_hours_summary
+        )
+        current_user_settings.daily_translate = (
+            translate
+            if translate is not None
+            else current_user_settings.daily_translate
+        )
+        current_user_settings.daily_process_read = (
+            process_read
+            if process_read is not None
+            else current_user_settings.daily_process_read
+        )
+
+        current_user_settings.daily_compare_titles = (
+            compare_titles
+            if compare_titles is not None
+            else current_user_settings.daily_compare_titles
+        )
+
+        current_user_settings.daily_title_provider = (
+            title_provider
+            if title_provider is not None
+            else current_user_settings.daily_title_provider
+        )
+
+        user.daily_sync_at = (
+            sync_datetime_utc
+            if sync_datetime_utc is not None
+            else user.daily_sync_at
+        )
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            logging.error(
+                "Error updating settings for %s: %s", current_user, str(e)
+            )
+            db.session.rollback()
+            return (
+                jsonify(
+                    message=f"Error updating settings for {current_user}: {e}"
+                ),
+                500,
+            )
+        return (jsonify(message="Settings updated successfully"), 200)
+
+
+@api_settings_blueprint.route("/daily/feed", methods=["PUT"])
+def update_daily_feed():
+    """Update daily feed settings."""
+    if not current_user.is_authenticated:
+        return jsonify({"message": "User not authenticated."}), 401
+
+    if request.method == "PUT":
+        feed = request.get_json()
+        print(feed)
+        feed_id = feed.get("id")
+        daily_enabled = feed.get("dailyEnabled")
+        if feed_id is None:
+            return jsonify({"message": "Missing feed ID."}), 400
+
+        # Fetch the feed by ID
+        existing_feed = Feed.query.filter_by(id=feed_id).first()
+
+        if not existing_feed:
+            return jsonify({"message": "Feed not found."}), 404
+
+        # Update the feed's active status
+        existing_feed.daily_enabled = daily_enabled
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            logging.error("Error updating feed %s: %s", feed_id, str(e))
+            db.session.rollback()
+            return (jsonify(message=f"Error updating feed: {str(e)}"), 500)
+
+        return jsonify(message="Feed updated successfully"), 200
 
 
 @api_settings_blueprint.route("/change_password", methods=["POST"])
@@ -152,7 +317,9 @@ def change_password():
         return (
             jsonify(
                 {
-                    "error": "New password must be different from the old password"
+                    "error": (
+                        "New password must be different from the old password"
+                    )
                 }
             ),
             400,
