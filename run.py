c0f5d9ffee243713876882cf3fdc7fd8dff3613c
defnone <141522332+defnone@users.noreply.github.com>
2024-08-27 10:17:15 +0700

    feat ✨: Add environment specific config

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from threading import Thread, Lock
 from flask import send_from_directory
 from waitress import serve
@@ -5,7 +6,9 @@ from app import create_app, db
 from app.models import Category, User
 from app.background_worker import run_scheduler
 
-app = create_app()
+config_name = os.getenv("FLASK_CONFIG", "default")
+app = create_app(config_name)
+
 BACKGROUND_WORKER_STARTED = False
 BACKGROUND_WORKER_LOCK = Lock()
 
@@ -15,6 +18,7 @@ def start_background_worker():
     Starts the background worker in a new thread if no background worker is
     currently running.
     """
+
     global BACKGROUND_WORKER_STARTED
     with BACKGROUND_WORKER_LOCK:
         if not BACKGROUND_WORKER_STARTED:
@@ -31,21 +35,26 @@ def robots_txt():
 
 if __name__ == "__main__":
     with app.app_context():
+        if config_name == "testing":
+            db.drop_all()
         db.create_all()
         # Check if category "Unnamed" exists
         user = User.query.first()
         if user:
             category = Category.query.filter_by(name="Unnamed").first()
+            start_background_worker()
             if not category:
                 # Create category "Unnamed"
                 category = Category(name="Unnamed", user_id=user.id)
                 db.session.add(category)
                 db.session.commit()
-        # Start the background worker
-        start_background_worker()
 
-    # Waitress is used as the production server
-    serve(app, host=app.config["FLASK_HOST"], port=app.config["FLASK_PORT"])
-
-    # Use the following line to run the app using Flask's development server
-    # app.run(debug=True, port=8000, host="0.0.0.0")
+    if os.getenv("ENV"):
+        if os.getenv("ENV") == "DEV":
+            app.run(debug=True, port=8000, host="0.0.0.0", use_reloader=False)
+    else:
+        serve(
+            app,
+            host=app.config["FLASK_HOST"],
+            port=app.config["FLASK_PORT"],
+        )
